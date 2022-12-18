a211adda42d08f7cbf2738b455b37aa936f643fe
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2022-12-18 00:38:11 -0400

    Add exercises with operators: 03-08 and 03-09

---
+++ Leccion03/03-08-EjercicioNumeroPar.js
@@ -0,0 +1,10 @@
+// Determinar si un número es par:
+
+let a = 10;
+
+if( a % 2 == 0) {
+    console.log( a + " es un número par"); // 10 es un número par
+}
+else {
+    console.log( a + " es un número impar");
+}
---
+++ Leccion03/03-09-EjercicioRevisarSiEsAdulto.js
@@ -0,0 +1,10 @@
+// Determinar si una persona es mayor de edad:
+
+let edad = 18, adulto = 18;
+
+if( edad >= adulto) {
+    console.log("Es un adulto");
+}
+else {
+    console.log("Es menor de edad");
+}
