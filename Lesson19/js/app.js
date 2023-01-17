6e6034d3443811067b2f19d38265f4ae0b2ee3a2
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-01-16 23:51:35 -0400

    [FEATURE] Lesson19 => Build a 'List of Persons' app: added styles and new function addPerson(). Test and closure

--- Lesson19/js/app.js
+++ Lesson19/js/app.js
@@ -1,14 +1,29 @@
 const personas = [
-    new Persona('Mariana', 'Milano'),
-    new Persona('Ramón', 'Milano')
+	new Persona('Mariana', 'Milano'),
+	new Persona('Ramón', 'Milano'),
 ];
 
-function mostrarPersonas(){
-    console.log('Mostrar personas:');
-    let texto = '';
-    for(let persona of personas){
-        console.log(persona);
-        texto += `<li>${persona.name} ${persona.lastname}</li>`;
-    }
-    document.getElementById('personas').innerHTML = texto;
+function mostrarPersonas() {
+	console.log('Mostrar personas:');
+	let texto = '';
+	for (let persona of personas) {
+		console.log(persona);
+		texto += `<li>${persona.name} ${persona.lastname}</li>`;
+	}
+	document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona() {
+	const formulario = document.forms['formulario'];
+	const name = formulario['name'];
+	const lastname = formulario['lastname'];
+
+	if (name.value != '' && lastname.value != '') {
+		const persona = new Persona(name.value, lastname.value);
+		console.log(persona);
+		personas.push(persona);
+		mostrarPersonas();
+	} else {
+		console.log('No information to add');
+	}
 }
